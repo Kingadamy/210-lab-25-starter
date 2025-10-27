1f2425c0ea4f4d32e8dc0ccb4029d5643ebfb9c7
Adam Yoonas <57850400+Kingadamy@users.noreply.github.com>
2025-10-26 17:49:01 -0700

    update

--- main.cpp
+++ main.cpp
@@ -18,15 +18,41 @@ struct mainResults {
 };
 
 // read all 20,000 strings from the file into a vector
-    vector<string> readStringsFromFile(const string& filename) {
-        vector<string> strings;
-        ifstream file(filename);
+    vector<string> readStringsFromFile(const string& filename) { 
+        vector<string> strings; 
+        ifstream file(filename); 
         string line;
         while (getline(file, line)) {
             strings.push_back(line);
         }
         return strings;
     }
+
+    // The first race is READING. The challenge is to read the 20,000 data elements into each of these data structures: vector, list, set
+
+    mainResults readingRace(const vector<string>& data) {
+        mainResults results = {0.0, 0.0, 0.0};
+
+        // vector reading
+        auto start = high_resolution_clock::now();
+        vector<string> vec(data);
+
+        auto end = high_resolution_clock::now();
+        results.vectorTime = duration_cast<milliseconds>(end - start).count();
+
+        //list reading
+        start = high_resolution_clock::now();
+        list<string> lst(data);
+
+        end = high_resolution_clock::now();
+        results.listTime = duration_cast<milliseconds>(end - start).count();
+
+        //set reading
+        start = high_resolution_clock::now();
+
+    
+
+
 
 
 int main() {
